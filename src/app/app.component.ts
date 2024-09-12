b7e095a37614b66ef685240f329f80be614fa1a2
Josh Lipscomb <joshlipscomb@gmail.com>
2024-09-12 09:36:05 -0400

    Baseline changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import accordion from "@uswds/uswds/js/usa-accordion";
+import characterCount from "@uswds/uswds/js/usa-accordion";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'uswds-angular';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // initialize
+    characterCount.on();
+    accordion.on();
+  }
+
+  // remove event listeners when the component un-mounts.
+  ngOnDestroy() {
+    characterCount.off();
+    accordion.off();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch(err => console.error(err))
+  .then(() => {
+    // Load USWDS script dynamically after bootstrapping
+    return loadUSWDSScript();
+  })
+  .catch(err => console.error('Error loading USWDS script:', err));
+
+  function loadUSWDSScript(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = 'assets/uswds/js/uswds.min.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = () => {
+      if (isDevMode()) {
+        console.log('USWDS script loaded');
+      }
+      resolve();
+    };
+    script.onerror = () => {
+      if (isDevMode()) {
+        console.error('Failed to load USWDS script');
+      }
+      reject();
+    };
+    document.head.appendChild(script);
+  });
+}
